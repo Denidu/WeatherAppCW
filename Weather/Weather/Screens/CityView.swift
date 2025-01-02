633fa65533de833dc6ce9f40ab0ffbebad1c7138
Denidu Gamage <denidugamage21@gmail.com>
2025-01-02 10:07:35 +0530

    Logic Changes

---
+++ Weather/Weather/Screens/CityView.swift
@@ -0,0 +1,18 @@
+//
+//  CityView.swift
+//  Weather
+//
+//  Created by Denidu Gamage on 2025-01-02.
+//
+
+import SwiftUI
+
+struct CityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CityView()
+}
