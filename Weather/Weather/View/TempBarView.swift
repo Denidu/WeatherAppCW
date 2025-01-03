c833a1ee04c82efe620951d5a0baa9e1e89c9e3f
Denidu Gamage <denidugamage21@gmail.com>
2025-01-03 22:03:18 +0530

    Add Functionalities

---
+++ Weather/Weather/View/TempBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TempBarView.swift
+//  Weather
+//
+//  Created by Denidu Gamage on 2025-01-03.
+//
+
+import SwiftUI
+
+struct TempBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TempBarView()
+}
